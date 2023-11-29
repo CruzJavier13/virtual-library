857305b3dce60996f90867bddbde11e054304b53
Erick Cruz <erickcroz2013@gmail.com>
2023-11-29 01:33:10 -0600

    The login form was designed

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,8 +68,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_library',
         'USER': 'root',
-        'PASSWORD': 'Cruz@2023',
-        'HOST': 'localhost',
+        'PASSWORD': 'cruz@2023',
+        'HOST': '127.0.0.1',
         'PORT': '3306',  # Puerto de MySQL por defecto
     }
 }
